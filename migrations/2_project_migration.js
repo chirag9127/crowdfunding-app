cf71653891752620e9ead5ecb2ca4e7b08f6c935
Chirag Mahapatra <chirag.mahapatra@gmail.com>
2023-08-13 11:32:05 -0700

    change constructor

--- migrations/2_project_migration.js
+++ migrations/2_project_migration.js
@@ -1,4 +1,4 @@
 var Project = artifacts.require("Project");
 module.exports = function(deployer) {
-    deployer.deploy(Project);
+    deployer.deploy(Project, "0xF2fdB3B26C0D1fbeCF91F2bbd3d19723dB425c31", "The next big app", "Description for the next big app", 1692556261, 200);
 };
